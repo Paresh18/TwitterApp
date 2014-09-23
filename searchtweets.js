3e7a8b150c2061466b6a6ae62c9220d9c8065f69
Paresh18 <paresh.shrivastava.jiit@gmail.com>
2014-09-23 17:06:12 +0530

    Commit1

---
+++ searchtweets.js
@@ -0,0 +1,42 @@
+
+/**
+ * Module dependencies.
+ */
+
+var qs = require('querystring')
+  , http = require('http')
+
+/**
+ * Search function.
+ *
+ * @param {String} search query
+ * @param {Function} callback
+ * @api public
+ */
+ 
+ module.exports = function search (query, fn) {
+  http.request({
+      host: 'search.twitter.com'
+    , path: '/search-home.json?' + qs.stringify({ q: query })
+  }, function (res) 
+       {
+    res.setEncoding('utf8');
+    var body = '';
+	
+    res.on('data', function (chunk) {
+      body += chunk;
+	  console.log(body);
+    });
+
+    res.on('end', function () {
+      try {
+        var obj = JSON.parse(body);
+      } catch (e) 
+	  {
+        return fn(new Error('Bad twitter response'));
+      }
+
+      fn(null, obj.results);
+    });
+  }).end()
+};
